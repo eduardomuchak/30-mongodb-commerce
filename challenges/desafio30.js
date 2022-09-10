fa5e95002c2c2740ec4e30f4ddd4bab4d0376316
Eduardo Muchak <eduardomuchak@gmail.com>
2022-09-10 19:03:31 -0300

    feat: :sparkles: requisito 30 finalizado
    Remova o campo curtidas do item Big Mac

---
+++ challenges/desafio30.js
@@ -0,0 +1,23 @@
+db.produtos.updateOne(
+  { 
+    nome: "Big Mac", 
+  },
+  { 
+    $unset: { 
+      curtidas: "", 
+    }, 
+  },
+);
+
+db.produtos.find(
+  { 
+    // nome: { 
+    //   $ne: "Big Mac", 
+    // }, 
+  },
+  { 
+    _id: 0, 
+    nome: 1, 
+    curtidas: 1, 
+  },
+);
